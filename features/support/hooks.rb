d510f2775ddc228f231175390b6463ba4f00e8a8
Matt <mattmcneil.ns@gmail.com>
2018-03-11 20:00:20 +0100

    remove leaving winow open after execution

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,21 +5,23 @@ end
 Before do
   Capybara.page.driver.browser.manage.window.maximize
 
-  Selenium::WebDriver::Driver.class_eval do
-    def quit
-      #STDOUT.puts "#{self.class}#quit: no-op"
-    end
-  end
 
-  Selenium::WebDriver::Chrome::Service.class_eval do
-    def stop
-      #STDOUT.puts "#{self.class}#stop: no-op"
-    end
-  end
 
-  Capybara::Selenium::Driver.class_eval do
-    def reset!
-    end
-  end
+  # Selenium::WebDriver::Driver.class_eval do
+  #   def quit
+  #     #STDOUT.puts "#{self.class}#quit: no-op"
+  #   end
+  # end
+  #
+  # Selenium::WebDriver::Chrome::Service.class_eval do
+  #   def stop
+  #     #STDOUT.puts "#{self.class}#stop: no-op"
+  #   end
+  # end
+  #
+  # Capybara::Selenium::Driver.class_eval do
+  #   def reset!
+  #   end
+  # end
 
 end
